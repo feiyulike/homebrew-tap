ffc46e6e10aabf789cd45216a4bfc073d4da527b
lihuaigu <lihuaigu@pzoom.com>
2023-04-03 14:13:18 +0800

    修复

--- Casks/iqiyi.rb
+++ Casks/iqiyi.rb
@@ -1,17 +1,16 @@
-require 'open-uri'
 cask "iqiyi" do
   version "latest"
   sha256 :no_check
 
-  url 'https://app.iqiyi.com/mac/player/index.html'
+  url do
+    pattern = /<a\s+class=".*?\bdl-installer\b.*?"\s+href="([\w:\/\.\-]+)"/
+    latest = curl("https://app.iqiyi.com/mac/player/index.html").match(regex).captures.first
+    latest
+  end
   name "爱奇艺视频"
   desc "Interactive media player"
   homepage "https://www.iqiyi.com/"
-  page_content = URI.open(url.to_s).read
-  download_link = page_content.match(/<a[^>]+class="[^"]*dl-installer[^"]*"[^>]+href="([^"]*)"[^>]*>/)[1]
-  installer script: {}
-  put download_link
-  appcast download_link
+
   livecheck do
     url "https://app.iqiyi.com/mac/player/index.html"
     strategy :page_match
