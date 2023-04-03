668b3647d558339325d3294cd96f3e3d918f1683
lihuaigu <lihuaigu@pzoom.com>
2023-04-03 14:32:38 +0800

    修复

--- Casks/iqiyi.rb
+++ Casks/iqiyi.rb
@@ -1,11 +1,15 @@
+require 'open-uri'
 cask "iqiyi" do
   version "latest"
   sha256 :no_check
 
   url do
     pattern = /https:\/\/.*\.dmg/
-    latest = curl("https://stc.iqiyipic.com/js/qiyiV2/20230220102415/jobs/pc/appDownloadByMac.js").match(regex)[2]
-    latest
+    url 'https://stc.iqiyipic.com/js/qiyiV2/20230220102415/jobs/pc/appDownloadByMac.js'
+    page_content = URI.open(url).read  
+    download_link = page_content.match(/https:\/\/.*\.dmg/)[2]
+    # latest = curl("https://stc.iqiyipic.com/js/qiyiV2/20230220102415/jobs/pc/appDownloadByMac.js").match(regex)[2]
+    download_link
   end
   name "爱奇艺视频"
   desc "Interactive media player"
