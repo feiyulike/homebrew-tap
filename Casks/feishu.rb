62ecdc9807735a9bd92b6133705bee5d78e06a02
lihuaigu <lihuaigu@pzoom.com>
2023-04-04 13:25:19 +0800

    1

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -6,8 +6,7 @@ cask "feishu" do
   puts latest_version
   sha256 :no_check
   def version
-    latest_version = JSON.parse(latest)['versions']['MacOS']['version_number'].split("@V")[1]
-    "#{latest_version}"
+    "6.1.5"
   end
   url do
     JSON.parse(latest)['versions']['MacOS']['download_link']
