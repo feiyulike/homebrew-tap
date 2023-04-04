3415d3e5275e1a977059c028da228f04cf8a4bd0
lihuaigu <lihuaigu@pzoom.com>
2023-04-04 15:04:33 +0800

    1

--- Casks/iqiyi.rb
+++ Casks/iqiyi.rb
@@ -2,9 +2,8 @@ require 'open-uri'
 cask "iqiyi" do
   project=URI.open('https://stc.iqiyipic.com/js/qiyiV2/appDownloadByMac_ver.js').read.match(/projectVersion:"(.*?)"/)[1]
   latest=URI.open("https://stc.iqiyipic.com/js/qiyiV2/#{project}/jobs/pc/appDownloadByMac.js").read.match(/a="(https?:\/\/[\w-]+\.[\w-]+\.[\w-]+\/[^\s]*iQIYIMedia_[^\s]*\.dmg)"/)[1]
-  puts latest
-  latest_version=URI.open('https://app.iqiyi.com/mac/player/index.html').read.match(/.*最新版本：([\d.]+)/i)[1]
-  version "#{latest_version}"
+  latest_version=URI.open('https://app.iqiyi.com/mac/player/index.html').read.match(/.*最新版本：([\d.]+)/)[1]
+  version "14.3.0"
   sha256 :no_check
   url "#{latest}"
   name "爱奇艺视频"
