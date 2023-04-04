1efbd77d815d49ca7562f409be6199e3f3c415e8
lihuaigu <lihuaigu@pzoom.com>
2023-04-04 16:08:27 +0800

    1

--- Casks/iqiyi.rb
+++ Casks/iqiyi.rb
@@ -4,8 +4,7 @@ cask "iqiyi" do
   project=URI.open('https://stc.iqiyipic.com/js/qiyiV2/appDownloadByMac_ver.js').read.match(/projectVersion:"(.*?)"/)[1]
   latest=URI.open("https://stc.iqiyipic.com/js/qiyiV2/#{project}/jobs/pc/appDownloadByMac.js").read.match(/a="(https?:\/\/[\w-]+\.[\w-]+\.[\w-]+\/[^\s]*iQIYIMedia_[^\s]*\.dmg)"/)[1]
   latest_version=URI.open('https://app.iqiyi.com/mac/player/index.html').read.match(/j-mac-version2.*最新版本：([\d.]+)/)[1]
-  puts latest_version
-  version "14.3.0"
+  version "#{latest_version}"
   sha256 :no_check
   url "#{latest}"
   name "爱奇艺视频"
