af880c0e0dd0e3378145b1398ef9dbb5ad5c9ddf
lihuaigu <lihuaigu@pzoom.com>
2023-04-04 11:42:59 +0800

    1

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'json'
 cask "feishu" do
   latest =URI.open('https://www.feishu.cn/api/downloads').read
-  url latest['versions']['MacOS']['download_link']
+  url latest['versions']['MacOS']
 
   name "Feishu"
   desc "Project management software"
