48d61b716d2c80a9505694f5cc3bbe45d028a6fe
lihuaigu <lihuaigu@pzoom.com>
2023-04-04 12:34:11 +0800

    1

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -3,7 +3,11 @@ require 'json'
 cask "feishu" do
   latest =URI.open('https://www.feishu.cn/api/downloads').read
   version do
-    JSON.parse(latest)['versions']['MacOS']['version_number']
+    url "https://www.feishu.cn/api/downloads"
+    regex(%r{/(\h+)/Feishu[._-]darwin[._-]x64[._-]v?(\d+(?:\.\d+)+)[._-]signed\.dmg}i)
+    strategy :page_match do |page|
+      page.scan(regex)
+          .map { |match| "#{match[1]},#{match[0]}" }
   end
   url do
     JSON.parse(latest)['versions']['MacOS']['download_link']
@@ -19,7 +23,7 @@ cask "feishu" do
     regex(%r{/(\h+)/Feishu[._-]darwin[._-]x64[._-]v?(\d+(?:\.\d+)+)[._-]signed\.dmg}i)
     strategy :page_match do |page|
       page.scan(regex)
-          .map { |match| "#{match[1]}" }
+          .map { |match| "#{match[1]},#{match[0]}" }
     end
   end
 
