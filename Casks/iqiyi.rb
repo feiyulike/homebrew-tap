277dea052fa301e765bd23f421427139f7365cca
lihuaigu <lihuaigu@pzoom.com>
2023-04-04 14:34:26 +0800

    1

--- Casks/iqiyi.rb
+++ Casks/iqiyi.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 cask "iqiyi" do
   project=URI.open('https://stc.iqiyipic.com/js/qiyiV2/appDownloadByMac_ver.js').read
-  puts project
+  puts project.match(/projectVersion:"(.*?)"/)[1]
   version "14.3.0"
   sha256 :no_check
   url "https://static-d.iqiyi.com/ext/common/iQIYIMedia_271.dmg"
